f12864c38c74e38903eb3c2989ca0c71674eeb12
Felix Wong <felix@fawong.com>
2011-06-24 01:23:36 -0700

    made code slightly more readable by breaking the plugin

--- src/main/java/com/fawong/PluginsManager/PluginsManager.java
+++ src/main/java/com/fawong/PluginsManager/PluginsManager.java
@@ -11,8 +11,6 @@ import java.util.HashMap;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.Date;
-import java.text.SimpleDateFormat;
 import org.bukkit.entity.Player;
 import org.bukkit.Server;
 import org.bukkit.event.Event.Priority;
@@ -20,9 +18,9 @@ import org.bukkit.event.Event.Type;
 import org.bukkit.event.Event;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
-import org.bukkit.plugin.PluginLoader;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.PluginManager;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.Command;
 
@@ -31,46 +29,22 @@ import org.bukkit.command.Command;
 *
 * @author fawong
 */
-public class PluginsManager extends JavaPlugin {
-	//private final PluginsManagerPlayerListener playerListener = new PluginsManagerPlayerListener(this);
-	private final HashMap<Player, Boolean> debugees = new HashMap<Player, Boolean>();
-	private static String config_folder_name = "plugins/PluginsManager";
-	private static String config_file_name = "pluginsmanager.settings";
-	private static String config_file_name_comment = "Plugins Manager Configuration File";
-	private static String output_toggle = "toggle";
-	private static String output_toogle_default_value = "on";
-	private static String output_file_name = "output_file_name";
-	private static String output_file_name_default_value = "CHANGE THIS VALUE";
-	private static String output_folder_name = "output_folder_name";
-	private static String output_folder_name_default_value = "CHANGE THIS VALUE";
-	private static String column_view = "column_view";
-	private static String column_view_default_value = "off";
-	private static String last_updated = "last_updated";
-	private static String last_updated_default_value = "on";
-	private static String plugin_name_branding = "plugin_name_branding";
-	private static String plugin_name_branding_default_value = "off";
-	private static String server_pretext = "server_pretext";
-	private static String server_pretext_default_value = "This server runs:";
-	private static String plugins_pretext = "plugins_pretext";
-	private static String plugins_pretext_default_value = "This server uses the following plugins:";
-	private static String css_file_name = "css_file_name";
-	private static String css_file_name_default_value = "CHANGE THIS VALUE";
-	private static File config_file = new File(config_folder_name, config_file_name);
-	private String output_toggle_value = "";
-	private String output_file_name_value = "";
-	private String output_folder_name_value = "";
-	private String column_view_value = "";
-	private String last_updated_value = "";
-	private String plugin_name_branding_value = "";
-	private String server_pretext_value = "";
-	private String plugins_pretext_value = "";
-	private String css_file_name_value = "";
-	private Logger mcl = Logger.getLogger("Minecraft");
-	private PluginManager pm; 
-	private PluginDescriptionFile pdFile;
-	private Plugin[] listofplugins;
-	private String[] nameofplugins;
-	private Properties prop = new Properties();
+public class PluginsManager extends JavaPlugin implements PluginsManagerSettings{
+	String toggle_value = "";
+	String output_file_name_value = "";
+	String output_folder_name_value = "";
+	String column_view_value = "";
+	String last_updated_value = "";
+	String plugin_name_branding_value = "";
+	String server_pretext_value = "";
+	String plugins_pretext_value = "";
+	String css_file_name_value = "";
+	Logger mcl = Logger.getLogger("Minecraft");
+	PluginManager pm = null;
+	PluginDescriptionFile pdFile = null;
+	Plugin[] listofplugins = null;
+	String[] nameofplugins = null;
+	Properties prop = new Properties();
 
 	public PluginsManager() {
 		// Custom initialisation code here
@@ -87,14 +61,18 @@ public class PluginsManager extends JavaPlugin {
 		pdFile = getDescription();
 		mcl.log(Level.INFO, "[PluginsManager]: " + pdFile.getName() + " version " + pdFile.getVersion() + " enabled");
 
+		// Set Executor file to use
+		getCommand("pmgr").setExecutor(new PMgrCommand(this));
+		getCommand("lp").setExecutor(new ListPluginsCommand(this));
+		getCommand("listplugins").setExecutor(new ListPluginsCommand(this));
+
 		// Call method to list plugins to a file.
 		if (loadSettings()) {
 			mcl.log(Level.INFO, "[PluginsManager]: settings have been loaded");
-			if (output_toggle_value.equals("on")) {
-				listPluginsToFile();
-			} else {
-				mcl.log(Level.INFO, "[PluginsManager]: Output toggle value off");
-				mcl.log(Level.INFO, "[PluginsManager]: Not outputting to file");
+			if (toggle_value.equals("off")) {
+				pm.disablePlugin(this);
+				mcl.log(Level.INFO, "[PluginsManager]: Toggle value off");
+				mcl.log(Level.INFO, "[PluginsManager]: Plugin disabled");
 			}
 		} else {
 			mcl.log(Level.SEVERE, "[PluginsManager]: Please configure the " + config_file_name + " file and reload the plugin");
@@ -109,7 +87,7 @@ public class PluginsManager extends JavaPlugin {
 		// EXAMPLE: Custom code, here we just output some info so we can check all is well
 		pdFile = getDescription();
 		mcl.log(Level.INFO, "[PluginsManager]: " + pdFile.getName() + " version " + pdFile.getVersion() + " disabled");
-		output_toggle_value = null;
+		toggle_value = null;
 		output_file_name_value = null;
 		output_folder_name_value = null;
 		column_view_value = null;
@@ -126,59 +104,8 @@ public class PluginsManager extends JavaPlugin {
 		prop = null;
 	}
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
-		if (isEnabled()) {
-			String commandName = command.getName().toLowerCase();
-			if ((commandName.equalsIgnoreCase("listplugins")) || (commandName.equalsIgnoreCase("lp"))) {
-				if (args.length != 0) {
-					boolean col = args[0].equalsIgnoreCase("column");
-					boolean row = args[0].equalsIgnoreCase("row");
-					if (col || row) {
-						String[] fpn = getFullPluginNames();
-						String sendstring = "";
-						for (int i = 0; i < fpn.length; i++) {
-							if (i < fpn.length - 1) {
-								if (col) {
-									sendstring = sendstring + fpn[i] + "\n";
-								}
-								else if (row) {
-									sendstring = sendstring + fpn[i] + ", ";
-								} else {
-									mcl.log(Level.SEVERE, "This should not happen.");
-								}
-							} else {
-								sendstring += fpn[i];
-							}
-						}
-						sender.sendMessage(sendstring);
-						return true;
-					} else {
-						sender.sendMessage("[PluginsManager]: You must specify either \"column\" or \"row\"");
-						return true;
-					}
-				} else {
-					sender.sendMessage("[PluginsManager]: You must specify an argument");
-					return true;
-				}
-			}
-			else if (sender instanceof Player) {
-				Player player = (Player) sender;
-				if ((commandName.equalsIgnoreCase("pmgr")) || (commandName.equalsIgnoreCase("pluginsmanager"))) { 
-					player.sendMessage("poke");
-				}
-				return true;
-			} else {
-				sender.sendMessage("[PluginsManager]: You need to be a player to run this command");
-				return true;
-			}
-		} else {
-			return false;
-		}
-	}
-
 	public void setDefaultSettings() {
-		prop.setProperty(output_toggle, output_toogle_default_value);
+		prop.setProperty(toggle, toggle_default_value);
 		prop.setProperty(output_folder_name, output_folder_name_default_value);
 		prop.setProperty(output_file_name, output_file_name_default_value);
 		prop.setProperty(column_view, column_view_default_value);
@@ -203,7 +130,7 @@ public class PluginsManager extends JavaPlugin {
 	public boolean loadSettings() {
 		try {
 			prop.load(new FileInputStream(config_file));
-			output_toggle_value = prop.getProperty(output_toggle);
+			toggle_value = prop.getProperty(toggle);
 			output_folder_name_value = prop.getProperty(output_folder_name);
 			output_file_name_value = prop.getProperty(output_file_name);
 			column_view_value = prop.getProperty(column_view);
@@ -212,14 +139,14 @@ public class PluginsManager extends JavaPlugin {
 			server_pretext_value = prop.getProperty(server_pretext);
 			plugins_pretext_value = prop.getProperty(plugins_pretext);
 			css_file_name_value = prop.getProperty(css_file_name);
-			if (output_toggle_value == null || output_folder_name_value == null || output_file_name_value == null ||
+			if (toggle_value == null || output_folder_name_value == null || output_file_name_value == null ||
 			column_view_value == null || last_updated_value == null || plugin_name_branding_value == null ||
 			server_pretext_value == null || plugins_pretext_value == null || css_file_name_value == null) {
 				mcl.log(Level.SEVERE, "[PluginsManager]: " + config_file_name + " file is not in the proper format");
 				return false;
 			} else {
 				mcl.log(Level.INFO, "[PluginsManager]: " + config_file_name + " file successfully loaded");
-				output_toggle_value = output_toggle_value.trim().toLowerCase();
+				toggle_value = toggle_value.trim().toLowerCase();
 				column_view_value = column_view_value.trim().toLowerCase();
 				last_updated_value = last_updated_value.trim().toLowerCase();
 				plugin_name_branding_value = plugin_name_branding_value.trim().toLowerCase();
@@ -230,82 +157,6 @@ public class PluginsManager extends JavaPlugin {
 			prop.clear();
 			setDefaultSettings();
 			return false;
-		}
-	}
-
-	private String[] getFullPluginNames() {
-		listofplugins = pm.getPlugins();
-		String[] returnstring = new String[listofplugins.length];
-		for (int i = 0; i < listofplugins.length; i++) {
-			pdFile = listofplugins[i].getDescription();
-			returnstring[i] = pdFile.getFullName();
-		}
-		return returnstring;
-	}
-
-	private String listFullPluginNames(String cvv) {
-		String returnstring = "";
-		nameofplugins = getFullPluginNames();
-		for (int j = 0; j < nameofplugins.length; j++) {
-			if (j < nameofplugins.length - 1) {
-				if (cvv.equalsIgnoreCase("off")) {
-					returnstring += nameofplugins[j] + ", ";
-				} else {
-					returnstring += nameofplugins[j] + "<br />\n";
-				}
-			} else {
-				returnstring += nameofplugins[j] + "<br /><br /><br /><br />\n";
-			}
-		}
-		return returnstring;
-	}
-
-	private String lastUpdatedDate() {
-		String returnstring = "";
-		if (last_updated_value.equalsIgnoreCase("on")) {
-			Date date = new Date();
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd @ HH:mm:ss");		
-			returnstring = "This page was generated on: " + sdf.format(date) + "\n";
-		}
-		return returnstring;
-	}
-
-	private String pluginNameBranding() {
-		String returnstring = "";
-		if (plugin_name_branding_value.equalsIgnoreCase("on")) {
-			pdFile = getDescription();
-			returnstring += "Using " + pdFile.getFullName() + "<br />\n";
-		}
-		return returnstring;
-	}
-
-	private void listPluginsToFile() {
-		String printtofile = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n";
-		printtofile += "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n";
-		printtofile += "<head>\n";
-		printtofile += "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\" />\n";
-		if (css_file_name_value.length() != 0) {
-			printtofile += "<link rel=\"stylesheet\" href=\"" + css_file_name_value + "\" type=\"text/css\" />\n";
-		}
-		printtofile += "<title>" + getServer().getName() + "</title>\n";
-		printtofile += "</head>\n";
-		printtofile += "<body>\n";
-		printtofile += "<strong>" + server_pretext_value + "</strong><br />\n" + getServer().getVersion() + "\n<br /><br />\n";
-		printtofile += "<strong>" + plugins_pretext_value + "</strong><br />\n";
-		printtofile += listFullPluginNames(column_view_value);
-		printtofile += lastUpdatedDate();
-		printtofile += pluginNameBranding();
-		printtofile += "</body>\n";
-		printtofile += "</html>\n";
-		try {
-			File file_to_output = new File(output_folder_name_value, output_file_name_value);
-			FileWriter fw = new FileWriter(file_to_output);
-			BufferedWriter out = new BufferedWriter(fw);
-			out.write(printtofile);
-			out.close();
-			mcl.log(Level.INFO, "[PluginsManager]: " + output_file_name_value + " successfully created");
-		} catch (IOException ioe) {
-			mcl.log(Level.SEVERE, "[PluginsManager]: " + output_file_name_value + " could not be created");
 		}
 	}
 
